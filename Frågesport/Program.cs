b34a1b17cbc142ed7da8c3945083472d086e14a6
Liam Esseen <Liam.esseen@elev.ga.ntig.se>
2023-09-19 09:08:04 +0200

    final

--- Frågesport/Program.cs
+++ Frågesport/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 
+
 string choice = "";
 
-Console.WriteLine("Hur mycket pengar har Liam Spenderat på Valorant?");
+Console.WriteLine("How much has this young fella spent on Valorant?");
 Console.WriteLine("2000kr [a] 10 000kr[b] 6 500kr[c]");
 
 choice = Console.ReadLine();
@@ -11,22 +12,99 @@ choice = choice.ToLower();
 if (choice == "a")
 {
 
-Console.WriteLine("Fel");
-Console.WriteLine("inte så lite");
+Console.WriteLine("Wrong");
+Console.WriteLine("Much more than that");
+Console.WriteLine("");    //empty rad
+Console.WriteLine("How old is Valorant");
+Console.WriteLine("1 year [a] 2 years [b] 3 years[c]");
 }
 
 
 else if (choice == "b")
 {
-Console.WriteLine("Fel");
-Console.WriteLine("inte så mycket");
+Console.WriteLine("Wrong");
+Console.WriteLine("Not that much");
+Console.WriteLine("");
+Console.WriteLine("How old is Valorant");
+Console.WriteLine("1 year [a] 2 years [b] 3 years[c]");
 }
 
 else if (choice == "c")
 {
-Console.WriteLine("Rätt ");
-Console.WriteLine("Jag kunde köpt en bil");
+Console.WriteLine("That's right");
+Console.WriteLine("Good job");
+Console.WriteLine("");
+Console.WriteLine("How old is Valorant");
+Console.WriteLine("1 year [a] 2 years [b] 3 years[c]");
 }
 
+string tab = "";
+
+Console.WriteLine("");
+
+tab = Console.ReadLine();
+tab = tab.ToLower();
+
+if (tab == "a")
+{
+Console.WriteLine("wrong");
+Console.WriteLine("What is a OP in valorant");
+Console.WriteLine("rifle [a] sniper [b] shotgun [c]");
+}
+
+
+else if (tab == "b")
+{
+Console.WriteLine("Wrong");
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("What is a OP in valorant");
+Console.WriteLine("rifle [a] sniper [b] shotgun [c]");
+}
+
+else if (tab == "c")
+{
+Console.WriteLine("lucky guess");
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("What is a OP in valorant");
+Console.WriteLine("rifle [a] sniper [b] shotgun [c]");
+}
+
+
+
+string boys = "";
+
+Console.WriteLine("");
+Console.WriteLine("");
+
+boys = Console.ReadLine();
+boys = boys.ToLower();
+
+if (boys == "a")
+{
+
+Console.WriteLine("Wrong");
+Console.WriteLine("");
+Console.WriteLine("Good try tho");
+Console.WriteLine("");
+}
+
+
+else if (boys == "b")
+{
+Console.WriteLine("hole in one");
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("Good job");
+}
+
+else if (boys == "c")
+{
+Console.WriteLine("Wrong");
+Console.WriteLine("");
+Console.WriteLine("Nice try tho");
+Console.WriteLine("");
+}
 
 Console.ReadLine();
